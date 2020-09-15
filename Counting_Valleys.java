345dffcdce69ff3a3133fc4b19c2f1fa26023658
omkar <shiroteomkmar6@gmail.com>
2020-09-16 02:48:35 +0530

    Minor Commit

--- Counting_Valleys.java
+++ Counting_Valleys.java
@@ -1,17 +1,6 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
-
-public class Solution {
+public class Counting_Valleys {
 public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
